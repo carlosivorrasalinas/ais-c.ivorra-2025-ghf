8322b32d141ff2e97288303d37dfb434b19b97e6
carlosivorrasalinas <98906502+carlosivorrasalinas@users.noreply.github.com>
2025-06-28 04:07:50 +0200

    Update FilmUIMultiBrowserTest.java

--- src/test/java/es/codeurjc/web/nitflex/e2e/web/FilmUIMultiBrowserTest.java
+++ src/test/java/es/codeurjc/web/nitflex/e2e/web/FilmUIMultiBrowserTest.java
@@ -1,5 +1,63 @@
 package es.codeurjc.web.nitflex.e2e.web;
 
-public class FilmUIMultiBrowserTest{
-    
+import java.time.Duration;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import es.codeurjc.web.nitflex.Application;
+
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FilmUIMultiBrowserTest {
+
+    @LocalServerPort
+    protected int port;
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeEach
+    void setupTest() {
+        String browser = System.getProperty("browser");
+        if (browser == null) {
+            throw new RuntimeException("Debes especificar el navegador con -Dbrowser=chrome/firefox/edge");
+        }
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalArgumentException("Navegador no soportado: " + browser);
+        }
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @DisplayName("Smoke test de FilmUI en navegador multiplataforma")
+    void multiBrowserTest() throws Exception {
+        driver.get("http://localhost:" + port + "/");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("header")));
+    }
 }
