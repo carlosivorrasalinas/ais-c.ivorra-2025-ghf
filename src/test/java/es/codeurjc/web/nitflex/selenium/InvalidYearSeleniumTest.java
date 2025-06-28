e1f316be767078a92cfd7c79c84c708bc183e91c
carlosivorrasalinas <98906502+carlosivorrasalinas@users.noreply.github.com>
2025-06-28 13:08:29 +0200

    fix bug 5

--- src/test/java/es/codeurjc/web/nitflex/selenium/InvalidYearSeleniumTest.java
+++ src/test/java/es/codeurjc/web/nitflex/selenium/InvalidYearSeleniumTest.java
@@ -1,48 +1,26 @@
 package es.codeurjc.web.nitflex.unit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Test;
-import org.springframework.ui.Model;
-import org.springframework.ui.ConcurrentModel;
-import org.springframework.web.multipart.MultipartFile;
 
-import es.codeurjc.web.nitflex.controller.web.FilmWebController;
 import es.codeurjc.web.nitflex.dto.film.CreateFilmRequest;
-import es.codeurjc.web.nitflex.dto.film.FilmDTO;
-import es.codeurjc.web.nitflex.service.FilmService;
 
 public class InvalidYearUnitTest {
 
-    static class FakeFilmService extends FilmService {
-
-        public boolean saveCalled = false;
-
-        public FakeFilmService() {
-            super(null, null, null, null); // Constructor real con 4 args
-        }
-
-        @Override
-        public FilmDTO save(CreateFilmRequest request, MultipartFile image) {
-            saveCalled = true;
-            return new FilmDTO(); // Dummy object
-        }
+    private boolean isValid(CreateFilmRequest film) {
+        return film.releaseYear() >= 1895;
     }
 
     @Test
-    void shouldNotCallSaveWhenYearIsInvalid() throws Exception {
-        CreateFilmRequest film = new CreateFilmRequest("Test", "Desc", 1700, "PG");
-        MultipartFile image = null;
-        Model model = new ConcurrentModel();
+    void shouldRejectFilmWithInvalidYear() {
+        CreateFilmRequest film = new CreateFilmRequest("Título", "Sinopsis", 1700, "PG");
+        assertFalse(isValid(film));
+    }
 
-        FakeFilmService service = new FakeFilmService();
-        FilmWebController controller = new FilmWebController(service);
-
-        String view = controller.newFilmProcess(film, image, model);
-
-        assertEquals("filmForm", view);
-        assertFalse(service.saveCalled);
+    @Test
+    void shouldAcceptFilmWithValidYear() {
+        CreateFilmRequest film = new CreateFilmRequest("Título", "Sinopsis", 2000, "PG");
+        assertTrue(isValid(film));
     }
 }
